8b642dd8337398d664326c1ce16d12993739770d
Nonnnyy <Nonnny07@gmail.com>
2022-08-10 12:19:58 +0100

    Added Spring Web Dependency

---
+++ src/main/java/org/nonny/com/SpringBootCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package org.nonny.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloneProjectApplication.class, args);
+	}
+
+}
